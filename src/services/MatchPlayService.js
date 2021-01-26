5522af78d8c38e02fb7c949be557dc9f0e9ad026
mayallzObject <mayallzobject@gmail.com>
2021-01-26 06:16:45 +0100

    sidebar basic styling

---
+++ src/services/MatchPlayService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const API_KEY = `b2f43580-5b76-11eb-b83d-33b6339dd693`;
+
+export const matchPlayApi = axios.create({
+  baseURL: `https://app.sportdataapi.com/api/v1/soccer`,
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
+export default {
+  getDutch1() {
+    return matchPlayApi.get(`/seasons?apikey=${API_KEY}&league_id=449`);
+  },
+  getDutch2() {
+    return matchPlayApi.get(`/seasons?apikey=${API_KEY}&league_id=450`);
+  }
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="home-view"></div>
+  <div class="home-view">
+    <div class="left-sidebar">
+      <div class="league-nav">
+        <div>
+          <h1>
+            sidenav>
+          </h1>
+        </div>
+        <button class="league-nav__item">
+          1. Eredivisie
+        </button>
+        <button class="league-nav__item">
+          2. Eerste Divisie
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
